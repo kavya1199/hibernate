179d39ac60b3a9a102edf0f25e05dc5e106fde02
kavya1199 <kavyarawte11@gmail.com>
2023-03-26 14:16:30 +0530

    My first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+// var a="vikash";
+// var b= "garduation";
+
+// console.log( `hy i am  ${a} and i have done ${b} in life `)
+// alert("hello vikash here how can i help you")
+//  confirm("vikash  here for help")
+// a=10;
+// var a;
+// let b;
+// a=20;
+// let c;
+// b=20;
+// const e=30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var a="hello world"
+// var result=a.charAt(1)
+
+// console.log(result);
+// console.log(a.charCodeAt(1))
+// console.log(a.concat("hello vikash here "))
+// console.log(a.length)
+// console.log(a.slice(0,2))
+// console.log(a.replace("h","E"))
+// console.log(a.toUpperCase())
+// console.log(a.toLowerCase())
+// console.log(a.split("hello"))
+// console.log(a.indexOf("world"))
+// console.log(a.search("l"))
+// console.log(a.includes("world"))
+// var a=number(prompt("hello vikash here"))
+// document.write(a)
+// console.log(a.length)
+// console.log(typeof a)
+// var e=5+1;
+// var d=5+"1";
+// console.log(d)
+// console.log(typeof d)
+// console.log(e)
+// console.log(typeof e)
+// var c=6
+// var a=prompt("enter the age ")
+// if (a>=18)
+// console.log("porsone is an adult");
+// else
+// console.log("porsone is an miner");
+
+
+
+//var b=prompt("enter the destination location")
+// var c=(prompt("enter the distance from source and destination"))
+// console.log("enter the distance beetween")
+  
+// console.log(b)
+// // console.log
+// var a=prompt("enter the price of the rice")
+// var b=prompt("enter the count of the rice ")
+// console.log("the price per pice is $",b/a)
+
+
+
+// var a="vikash"
+// while (5) {
+    
+    
+//var a=prompt("enter the price of the saree")
+//var b=prompt("enter the count of saree")
+//console.log("the price per pice is", b+a)
+//console.log(22)
+//prompt("enter the number")
+//alert("hello my name is vikash")
+//confirm("are u ther akash ")
+//console.log(typeof undefined)
+var a=`vikash`;
+var b= 'bussunesman';
+console.log(`${a}  is a ${b} saree`)
+
+
